36d872ad65096b31489e887c538f558774924939
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-08-05 16:31:57 +0300

    Rukkola DevJobs Site
    Started a job search project for it specialists.
    
    * Added header and started styling using SCSS preprocessor.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+        <div className="container">
+            <h1>Rukkola!</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header;
