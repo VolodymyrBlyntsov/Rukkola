a5f6f661cc343b1afee3816320946c9742f55258
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-08-05 19:27:56 +0300

    Rukkola DevJobs App
    * Added future page routing using the react-router-dom library.
    
    * Started implementing the functionality of future job search on the site.
    
    * Added two icons to match the search context, using the remixicon library.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import JobsList from './components/Jobs/JobsList';
+import JobDetails from './components/JobDetails/JobDetails';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <Header />  
+      <Routes>
+        <Route path='/' element={ <Navigate to='/jobs' /> } />
+        <Route path='/jobs' element={ <JobsList /> } />
+        <Route path='/jobs/:position' element={ <JobDetails /> } />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Jobs/JobsList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const JobsList = () => {
+  return (
+    <section className="job__list">
+      <div className="container">
+        <div className="job__list__wrapper">
+          <div className="search__panel">
+            <div className="search__panel-01">
+              <span>
+                <i class="ri-search-line"></i>
+              </span>
+              <input type="text" placeholder='Search by title, companies' />
+            </div>
+            <div className="search__panel-02">
+              <span>
+                <i class="ri-map-pin-2-line"></i>
+              </span>
+              <input type="text" placeholder='Search by location' />
+              <button>Search</button>
+            </div>
+            <div className="search__panel__-03">
+              <select>
+                <option> Filter job by </option>
+                <option value="full-time"> Full Time </option>
+                <option value="part-time"> Part Time </option>
+                <option value="freelance"> Freelance </option>
+                <option value="contract"> Contract </option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default JobsList
