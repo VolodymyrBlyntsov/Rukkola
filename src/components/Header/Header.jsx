165f609757038b6517072c0b4573a7e54797848f
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-08-08 22:58:59 +0300

    Rukkola DevJobs App
    * Implemented application responsiveness (mobile, tablet).
    
    * Changed slightly the design of the job display.
    
    * "Rukkola" - now a button to go to the home page from anywhere on the site.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Header.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none'
+  }
   return (
     <header className="header">
         <div className="container">
-            <h1>Rukkola!</h1>
+            <button className='siteBtn'><Link to="/jobs" style={linkStyle}>Rukkola!</Link></button>
         </div>
     </header>
   )
