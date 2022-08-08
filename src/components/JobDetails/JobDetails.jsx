7f4ad2fbdc06c094d87abc27533f55b00ae4643e
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-08-08 22:18:29 +0300

    Rukkola DevJobs App
    * Developed the functionality of the page where the information about the vacancy is posted.
    
    * The design of the job page has been worked out, as well as optimized.
    
    * Minor changes in scss files.

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import jobs from '../../data/data';
+import './JobDetails.scss';
 
 const JobDetails = () => {
+
+  const {position} = useParams()
+  const job = jobs.find(job => job.position === position)
+
   return (
-    <div>JobDetails</div>
+    <section>
+      <div className="container">
+        <div className="details__wrapper">
+          <div className="details__top">
+            <img src={job.logo} alt="" />
+            <div>
+              <h1> {job.company} </h1>
+            </div>
+            <button>
+              <Link to={job.companySite}> Company Site </Link>
+            </button>
+          </div>
+
+          <div className="job__details">
+            <div className="about__job">
+              <div>
+                <h6> {job.postedAt} - {job.contract} </h6>
+                <h1> {job.position} </h1>
+                <span> {job.location} </span>
+              </div>
+              <div className="btn"> Apply </div>
+            </div>
+
+            <p className='job__desc'>{job.desc}</p>
+
+            <div className="requirements">
+              <h1> Requirements </h1>
+              <p> {job.requirements.reqTitle} </p>
+              <ul className="requirements__item">
+                {
+                  job.requirements.reqItem.map((item, index) => (
+                    <li key={index}> {item} </li>
+                  ))
+                }
+              </ul>
+            </div>
+
+            <div className="responsibility">
+              <h1> What Will you do? </h1>
+              <p> {job.responsibility.resTitle} </p>
+              <ol type="1" className="responsibility__item">
+                {
+                  job.responsibility.resItem.map((item, index) => (
+                    <li key={index}> {item} </li>
+                  ))
+                }
+              </ol>
+            </div>
+
+         </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default JobDetails
+export default JobDetails;
